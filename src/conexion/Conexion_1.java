106ce6714fc10f943fd4a33cd2079a62f6649d74
LuisSilvestre95 <luissilvestre70@gmail.com>
2023-11-13 16:52:17 -0500

    Conexion

--- src/conexion/Conexion_1.java
+++ src/conexion/Conexion_1.java
@@ -1,22 +1,49 @@
 package conexion;
 
+/**
+ *
+ * @author Luis Silvestre
+ */
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
-/**
- *
- * @author ediso
- */
-public class Conexion {
+public class Conexion_1 {
 
-    //conexion local
-    public static Connection conectar() {
+    // Constantes para la conexión a la base de datos
+    private static final String URL = "jdbc:mysql://localhost:3306/ProyectoBaseDeDatos";
+    private static final String USER = "root";
+    private static final String PASSWORD = "Orion1";
+
+    public static void main(String[] args) {
+        Connection conn = null;
         try {
-            Connection cn = DriverManager.getConnection("jdbc:mysql://localhost/bd_sistema_ventas", "root", "1234");
+            // Establece la conexión a la base de datos MySQL.
+            conn = DriverManager.getConnection(URL, USER, PASSWORD);
+            // Realiza operaciones con la conexión si es necesario
+        } catch (SQLException e) {
+            e.printStackTrace();
+            System.err.println("Error al conectar a la base de datos: " + e.getMessage());
+        } finally {
+            // Cierra la conexión en el bloque finally para asegurar que se cierre correctamente.
+            try {
+                if (conn != null && !conn.isClosed()) {
+                    conn.close();
+                }
+            } catch (SQLException e) {
+                e.printStackTrace();
+                System.err.println("Error al cerrar la conexión: " + e.getMessage());
+            }
+        }
+    }
+
+    // Conexión local
+    public static Connection conectar() {
+        try (Connection cn = DriverManager.getConnection("jdbc:mysql://localhost/bd_sistema_ventas", "root", "1234")) {
             return cn;
         } catch (SQLException e) {
-            System.out.println("Error en la conexion local " + e);
+            e.printStackTrace();
+            System.out.println("Error en la conexión local: " + e.getMessage());
         }
         return null;
     }
